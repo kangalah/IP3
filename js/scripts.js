2eea341569aa756a57e45f627ae5f43cec4e267e
joan <nyongesajoannanjala@gmail.com>
2020-05-11 16:18:40 +0300

    added an alert function

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,18 @@
     $(this).children('.show').slidetoggle(1400);
   });
 
-  
-  $(".card").hover(function () {
-    $(this).children(".card-1").fadeToggle(500,);
-  });
 
-
+    $(document).ready(function(){
 $('.submit').click(function () {
-  var name = $('#mce-FNAME').val();
-  var email = $('#mce-EMAIL').val();
-  var message = $('#mce-MESSAGE');
-  var key ='c8fbd9a0cf3958e02be2822e55cfbfc5-us4';
+  var name = $('#b_name').val();
+  var email = $('#b_email').val();
+  var message = $('#b_comment');
+  // var key ='c8fbd9a0cf3958e02be2822e55cfbfc5-us4';
   if (name == '' || email == '' || message == '') {
       alert('Please make sure you have filled in the form correctly!');
   } else {
       alert(' Hi ' + name + ' We have received your message. Thank you for reaching out to us.');
   }
   event.preventDefault ();
+});
 });
